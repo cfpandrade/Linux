5f0e130520fee1e9bfdb5d78b04d1f0142fa0c6b
CLO <cfpandrade@gmail.com>
2023-02-03 00:27:10 +0000

    Update whichSystem.py

--- apps/whichSystem.py
+++ apps/whichSystem.py
@@ -3,31 +3,59 @@
 
 import subprocess
 import sys
+from termcolor import colored
+
+
+def print_colored(string, color):
+    print(colored(string, color))
 
 if len(sys.argv) != 2:
-    print("\n[!] Uso: python3 " + sys.argv[0] + " <direccion-ip>\n")
+    print_colored("\n[!] Uso: python3 " + sys.argv[0] + " <direccion-ip>\n", "red")
     sys.exit(1)
 
 ip_address = sys.argv[1]
 result = subprocess.run(["ping", "-c", "1", ip_address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-ttl = int(float(result.stdout.split()[7]))
+stdout = result.stdout.decode("utf-8")
+
+try:
+    ttl_index = stdout.index("ttl=") + 4
+    ttl = int(stdout[ttl_index:stdout.index(" ", ttl_index)])
+except ValueError:
+    ttl = None
 
 if ttl >= 0 and ttl <= 64:
     os_name = "Linux"
+    color = "blue"
+    print_colored("\n                                    ", "cyan")
+    print_colored(" █████        ███                                   ", "cyan")
+    print_colored("░░███        ░░░                                    ", "cyan")
+    print_colored(" ░███        ████  ████████   █████ ████ █████ █████", "cyan")
+    print_colored(" ░███       ░░███ ░░███░░███ ░░███ ░███ ░░███ ░░███ ", "cyan")
+    print_colored(" ░███        ░███  ░███ ░███  ░███ ░███  ░░░█████░  ", "cyan")
+    print_colored(" ░███      █ ░███  ░███ ░███  ░███ ░███   ███░░░███ ", "cyan")
+    print_colored(" ███████████ █████ ████ █████ ░░████████ █████ █████", "cyan")
+    print_colored("░░░░░░░░░░░ ░░░░░ ░░░░ ░░░░░   ░░░░░░░░ ░░░░░ ░░░░░ ", "cyan")
+    print_colored("\n\n", "cyan")
 elif ttl >= 65 and ttl <= 128:
     os_name = "Windows"
+    color = "yellow"
+    print_colored("\n                                   ", "cyan")
+    print_colored(" █████   ███   █████  ███                 █████                                 ", "cyan")
+    print_colored("░░███   ░███  ░░███  ░░░                 ░░███                                  ", "cyan")
+    print_colored(" ░███   ░███   ░███  ████  ████████    ███████   ██████  █████ ███ █████  █████ ", "cyan")
+    print_colored(" ░███   ░███   ░███ ░░███ ░░███░░███  ███░░███  ███░░███░░███ ░███░░███  ███░░  ", "cyan")
+    print_colored(" ░░███  █████  ███   ░███  ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░░█████ ", "cyan")
+    print_colored("  ░░░█████░█████░    ░███  ░███ ░███ ░███ ░███ ░███ ░███ ░░███████████   ░░░░███", "cyan")
+    print_colored("    ░░███ ░░███      █████ ████ █████░░████████░░██████   ░░████░████    ██████ ", "cyan")
+    print_colored("     ░░░   ░░░      ░░░░░ ░░░░ ░░░░░  ░░░░░░░░  ░░░░░░     ░░░░ ░░░░    ░░░░░░  ", "cyan")
+    print_colored("\n\n", "cyan")
 else:
     os_name = "Not Found"
+    color = "red"
 
-print("%s (ttl -> %d): %s" % (ip_address, ttl, os_name))
+if ttl is None:
+    print_colored("%s (ttl -> Not Found): %s" % (ip_address, os_name), color)
+else:
+    print_colored("%s (ttl -> %d): %s" % (ip_address, ttl, os_name), color)
 
-#  ______________________________
-# < developed by: Carlos Andrade >
-#  ------------------------------
-#         \   ^__^
-#          \  (oo)\_______
-#             (__)\       )\/\
-#                 ||----w |
-#                 ||     ||
-
-
+print_colored("\n\nDeveloped by: Carlos Andrade", "green")
