f4590842bc9c3c3a36b78b72658783b1a9257cc9
cfpandrade <cfpandrade@gmail.com>
2025-05-26 01:01:56 +0100

    Update apps

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ echo ""
 echo ""
 
 header "Installing requirements"
-sudo apt -y install git vim xcb fonts-powerline tmux zsh-autosuggestions mawk sed htop neovim ncdu snapd default-mysql-client imagemagick
+sudo apt -y install git vim xcb fonts-powerline tmux zsh-autosuggestions mawk sed htop neovim ncdu snapd imagemagick mariadb-client
 sudo apt -y install acl fortune cowsay locate curl 
 sudo apt -y install software-properties-common
 sudo apt -y install docker.io docker-compose docker-clean
